5a02da888a1ea8d990da53ccbabc48bd7df53e6a
DinaraKhislis <khisameeva.dinara@gmail.com>
2023-03-12 23:31:24 +0300

    SQL_HW.sql

---
+++ SQL_HW.sql
@@ -0,0 +1,45 @@
+INSERT INTO "/* 1. Вывести всех работников чьи зарплаты есть в базе, вместе с зарплатами */
+select employees.employee_id, salary2.salary2_id
+from employees
+inner join employee_salary on employees.id=employee_salary.employee_id
+inner join salary2 on employee_salary.salary2_id=salary2.id" (employee_id,salary2_id) VALUES
+	 ('Tammy Galindo',1600),
+	 ('Reggie Batey',1300),
+	 ('Jill Anspach',1800),
+	 ('Isaac Walters',2200),
+	 ('Karen Santiago',1300),
+	 ('Miguel Fletcher',1100),
+	 ('Michael Diaz',1900),
+	 ('Mary Dowell',2200),
+	 ('Ronda Colley',1300),
+	 ('Marie Thomas',1000);
+INSERT INTO "/* 1. Вывести всех работников чьи зарплаты есть в базе, вместе с зарплатами */
+select employees.employee_id, salary2.salary2_id
+from employees
+inner join employee_salary on employees.id=employee_salary.employee_id
+inner join salary2 on employee_salary.salary2_id=salary2.id" (employee_id,salary2_id) VALUES
+	 ('Charlene Little',1600),
+	 ('Clara Poore',1400),
+	 ('Lacy Racicot',2000),
+	 ('David Macias',2000),
+	 ('Ryan Morgan',1500),
+	 ('Daniel Patterson',2400),
+	 ('Lucius Patterson',1700),
+	 ('Betty Wells',1100),
+	 ('Gregory Magelssen',2500),
+	 ('Courtney Alston',1700);
+INSERT INTO "/* 1. Вывести всех работников чьи зарплаты есть в базе, вместе с зарплатами */
+select employees.employee_id, salary2.salary2_id
+from employees
+inner join employee_salary on employees.id=employee_salary.employee_id
+inner join salary2 on employee_salary.salary2_id=salary2.id" (employee_id,salary2_id) VALUES
+	 ('Michael Mark',1100),
+	 ('Robert Harian',1800),
+	 ('Fred Spencer',1200),
+	 ('Jessica Manns',1300),
+	 ('Mattie Kelley',1100),
+	 ('Amber Holcomb',2200),
+	 ('Rickey Jones',1500),
+	 ('Nancy Bravo',2200),
+	 ('Chester Baird',2500),
+	 ('Yoshiko Miller',2300);
